4e9506308462571a1fe8f532252c7e47eebf61b7
Gilles Pilon <gillespilon13@gmail.com>
2020-08-26 21:05:28 -0500

    Initial commit

---
+++ print_function.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python3
+
+'''
+print_function.py
+'''
+
+
+print('One,', end=' ')
+print('\'two\',', end=' ')
+print('"three",', end=' ')
+print('four,', end=' ')
+print('\\five\\', end=' ')
+print('once,', end=' ')
+print('\'I\'', end=' ')
+print('caught a', end=' ')
+print('\n\tfish', end=' ')
+print('\'//alive\\\\\'')
