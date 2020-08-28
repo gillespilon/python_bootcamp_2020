9303fced5b4c1ab910ff3170b0f6a357c560a8e7
Gilles Pilon <gillespilon13@gmail.com>
2020-08-27 19:01:46 -0500

    Determine my version of the correct solution

--- print_function.py
+++ print_function.py
@@ -5,16 +5,8 @@ print_function.py
 '''
 
 
-print('One,', end=' ')
-print('\'two\',', end=' ')
-print('"three",', end=' ')
-print('four,', end=' ')
-print('\\five\\', end=' ')
-print('once,', end=' ')
-print('\'I\'', end=' ')
-print('caught a', end=' ')
-print('\n\tfish', end=' ')
-print('\'//alive\\\\\'')
-
-print('One,\'two\',\"three\",four,\\five\\\n\tonce,\'I\' caught a\
- fish\n\t\t\'//alive\\\\\'')
+print(
+    "One, 'two', \"three\", four,\
+    \n\t\\five\\ once,'I' caught a\
+    \n\t\tfish '//alive\\\\'"
+)
