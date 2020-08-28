dfad0869c2b7c0cf392fad8d2cba76bbe42c8557
Gilles Pilon <gillespilon13@gmail.com>
2020-08-28 14:49:27 -0500

    Initial commit

---
+++ question_01.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python3
+
+'''
+question_01.py
+'''
+
+
+import math
+
+
+# Ask the user for the radius of a circle and then print the area
+
+radius_of_circle = float(input('What is the radius of the circle? > '))
+# print(radius_of_circle)
+area_of_circle = math.pi * radius_of_circle**2
+# print(area_of_circle)
+print('You entered a radius of ', radius_of_circle)
+print('The area of the circle is ', area_of_circle)
