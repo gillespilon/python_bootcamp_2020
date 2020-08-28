04509f5be6d655a427eb9c0bca52c563a8b1022e
Gilles Pilon <gillespilon13@gmail.com>
2020-08-28 10:25:06 -0500

    Add valid, invalid examples

--- print_function.py
+++ print_function.py
@@ -5,6 +5,19 @@ print_function.py
 '''
 
 
+# Valid
+print(7/3)
+print(8/2)
+print(8//2)
+print(2**8)
+print('Hello world')
+print('how are \nyou today?')
+
+# Invalid
+# print(World)
+# print('how's it going?')
+
+# Exercise
 print(
     "One, 'two', \"three\", four,\
     \n\t\\five\\ once,'I' caught a\
