17d3c98852a3d9eb9f2200fcc8aec65a14547e6b
Gilles Pilon <gillespilon13@gmail.com>
2020-08-29 18:18:27 -0500

    Add to docstring, delete blank line

--- area_of_circle.py
+++ area_of_circle.py
@@ -2,6 +2,8 @@
 
 '''
 area_of_circle.py
+
+./area_of_circle.py
 '''
 
 
@@ -9,7 +11,6 @@ import math
 
 
 # Ask the user for the radius of a circle and then print the area
-
 radius_of_circle = float(input('What is the radius of the circle? > '))
 # print(radius_of_circle)
 area_of_circle = math.pi * radius_of_circle**2
