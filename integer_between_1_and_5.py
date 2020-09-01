64a033bce21a3a41d3ae0237663ce08d3c0d8591
Gilles Pilon <gillespilon13@gmail.com>
2020-09-01 17:32:09 -0500

    Initial commit

---
+++ integer_between_1_and_5.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python3
+
+'''
+integer_betwee_1_and_5.py
+
+./integer_betwee_1_and_5.py
+'''
+
+
+input_value = int(input('Give me a number between 1 and 5, inclusive. > '))
+if input_value == 1:
+    print('one')
+elif input_value == 2:
+    print('two')
+elif input_value == 3:
+    print('three')
+elif input_value == 4:
+    print('four')
+elif input_value == 5:
+    print('five')
+else:
+    print('That is not a number between 1 and 5, inclusive.')
